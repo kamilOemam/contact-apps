135f7f7b7c09121476922f5c96202748f857012f
kamilOemam <kamilumam@gmail.com>
2022-12-06 16:51:41 +0700

    initial commit

---
+++ src/data.js
@@ -0,0 +1,25 @@
+const getData = () => {
+    return [
+        {
+            id: 1, 
+            name: 'Dimas Saputera',
+            tag: 'dimasmds', 
+            imageUrl: '/images/dimasmds.jpeg',
+        },
+        {
+            id: 2, 
+            name: 'Arif Faizin',
+            tag: 'arifaizin', 
+            imageUrl: '/images/arifaizin.jpeg',
+        },
+        {
+            id: 1, 
+            name: 'Rahmat Fajri',
+            tag: 'rfajri27', 
+            imageUrl: '/images/rfajri27.jpeg',
+        },
+        
+    ];
+}
+
+export {getData};
